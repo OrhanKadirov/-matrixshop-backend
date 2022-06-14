2a3cc9760f55ecf51ce559b24e8d398673e60c7c
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-06-14 10:54:13 +0200

    index.js erstelt und die backend im laufen gebracht

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.json({ msg: "Matrix Shop Home API up in runing" });
+});
+
+mongoose.connect(process.env.MONGO_CONNECTION).then(() => {
+  app.listen(() => {
+    console.log(`Matrix Shop API is listening on http://localhost:${port}`);
+  });
+});
